75d169c61e1f67aed8435fad64eab1cb8c5e34cf
Beast9989 <ddosmaker580@gmail.com>
2024-07-25 22:49:24 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7332871906:AAEYS6m2N60C0zjCDZo_aWlVdhRWIplgRPI')
 
 # Admin user IDs
-admin_id = ["your id"]
+admin_id = ["5775930884"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
