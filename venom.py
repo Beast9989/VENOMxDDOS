f12210296ff5ecef2942eb47f2efd886fab1dd9c
Beast9989 <ddosmaker580@gmail.com>
2024-07-25 22:51:24 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7332871906:AAEYS6m2N60C0zjCDZo_aWlVdhRWIplgRPI'
+ADMIN_ID = '5775930884'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
